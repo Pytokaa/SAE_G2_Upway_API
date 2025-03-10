a4916c4f90ee0fffac14f10041df22e39802c9af
Pytokaa <elioth.rob@proton.me>
2025-03-10 15:41:32 +0100

    Ajout du début de la class produit (a completer)

--- SAE_G2_Upway_API/Models/EntityFramework/Produit.cs
+++ SAE_G2_Upway_API/Models/EntityFramework/Produit.cs
@@ -1,6 +1,35 @@
-namespace SAE_G2_Upway_API.Models.EntityFramework;
 
-public class Produit
+
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+
+//il faut encore ajouter les id pour les photos et les marques
+
+namespace SAE_G2_Upway_API.Models.EntityFramework;
+[Table("produit")]
+public partial class Produit
 {
+    [Key]
+    [Column("idproduit")]
+    public int Idproduit { get; set; }
+    
+    
+    [Column("nomproduit")]
+    [StringLength(100)]
+    public string Nomproduit { get; set; }
+    
+    
+    [Column("prixproduit")]
+    public double Prixproduit { get; set; }
+    
+    [Column("descriptionproduit")]
+    [StringLength(200)]
+    public string Descriptionproduit { get; set; }
+    
+    [Column("quantiteproduit")]
+    public int Quantiteproduit { get; set; }   
     
 }
