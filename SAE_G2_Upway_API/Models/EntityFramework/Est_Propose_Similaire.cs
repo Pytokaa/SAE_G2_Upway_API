25acca66f2d63e3bf7e2b1ec954aee2e72b1220d
stapleta <stapleta@D253-S21>
2025-03-12 11:33:26 +0100

    changement en partial et virtual

--- SAE_G2_Upway_API/Models/EntityFramework/Est_Propose_Similaire.cs
+++ SAE_G2_Upway_API/Models/EntityFramework/Est_Propose_Similaire.cs
@@ -17,10 +17,10 @@ namespace SAE_G2_Upway_API.Models.EntityFramework
 
         [ForeignKey(nameof(IdAccessoire))]
         [InverseProperty(nameof(Accessoire.LesCommandes))]
-        public Accessoire LAccessoire { get; set; } = null!;
+        public virtual Accessoire LAccessoire { get; set; } = null!;
 
         [ForeignKey(nameof(IdCommande))]
         [InverseProperty(nameof(Commande.LesSimilaires))]
-        public Commande LaCommande { get; set; } = null!;
+        public virtual Commande LaCommande { get; set; } = null!;
     }
 }
