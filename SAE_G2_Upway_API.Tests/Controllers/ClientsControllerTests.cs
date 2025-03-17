26999c30e4131e841b0068367ae317dd56d41a1d
Stapleton Alexis <alexis.stapleton@etu.univ-smb.fr>
2025-03-17 17:34:55 +0100

    Baaaakaaaa

---
+++ SAE_G2_Upway_API.Tests/Controllers/ClientsControllerTests.cs
@@ -0,0 +1,17 @@
+using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SAE_G2_Upway_API.Controllers;
+
+namespace SAE_G2_Upway_API.Tests.Controllers;
+
+[TestClass]
+[TestSubject(typeof(ClientsController))]
+public class ClientsControllerTests
+{
+
+    [TestMethod]
+    public void METHOD()
+    {
+        
+    }
+}
