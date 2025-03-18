6215aba9e65ccef44aec5c333772c5466d7defc0
Stapleton Alexis <alexis.stapleton@etu.univ-smb.fr>
2025-03-18 11:45:20 +0100

    ajout des tests

---
+++ SAE_G2_Upway_APITests/Controllers/ProduitsControllerTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SAE_G2_Upway_API.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE_G2_Upway_API.Controllers.Tests
+{
+    [TestClass()]
+    public class ProduitsControllerTests
+    {
+        [TestMethod()]
+        public void ProduitsControllerTest()
+        {
+            
+        }
+
+        [TestMethod()]
+        public void GetProduitsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetProduitByIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetProduitByNameTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PutProduitTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PostProduitTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteProduitTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
