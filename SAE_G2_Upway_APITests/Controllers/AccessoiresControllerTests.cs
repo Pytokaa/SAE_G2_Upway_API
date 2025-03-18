b432d3357b4da9249e3d29d19bee6dd1443a3475
Stapleton Alexis <alexis.stapleton@etu.univ-smb.fr>
2025-03-18 08:18:21 +0100

    recreation des tests

---
+++ SAE_G2_Upway_APITests/Controllers/AccessoiresControllerTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SAE_G2_Upway_API.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE_G2_Upway_API.Controllers.Tests
+{
+    [TestClass()]
+    public class AccessoiresControllerTests
+    {
+        [TestMethod()]
+        public void AccessoiresControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAccessoiresTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAccessoireByIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAccessoireByNameTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PutAccessoireTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PostAccessoireTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteAccessoireTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
