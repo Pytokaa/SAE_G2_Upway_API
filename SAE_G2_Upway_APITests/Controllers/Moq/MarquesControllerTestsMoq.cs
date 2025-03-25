25cebad91edc70c46c39205b29d61ca42e70fe59
Jules Pigné <jules.pigne@etu.univ-smb.fr>
2025-03-25 17:41:14 +0100

    Suppression d'un dossier test chelou et début tests moq marque

---
+++ SAE_G2_Upway_APITests/Controllers/Moq/MarquesControllerTestsMoq.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SAE_G2_Upway_API.Controllers;
+using SAE_G2_Upway_API.Models.EntityFramework;
+using SAE_G2_Upway_API.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE_G2_Upway_APITests.Controllers.Moq
+{
+    [TestClass()]
+    public class MarquesControllerTestsMoq
+    {
+        private MarquesController? controller;
+        private Mock<IDataRepository<Marque, Marque>> mockRepository;
+
+        [TestInitialize]
+        public void Init()
+        {
+            mockRepository = new Mock<IDataRepository<Marque, Marque>>();
+            controller = new MarquesController(mockRepository.Object);
+        }
+
+        [TestMethod]
+        public void GetAllMarquesMoq_Succeed()
+        {
+            //Arrange
+            List<Marque> marques = new List<Marque>([
+                new Marque(1,"Giant"),
+                new Marque(2,"Btwin"),
+                new Marque(3, "Moustache")
+                ]);
+            mockRepository.Setup(x => x.GetAllAsync().Result).Returns(marques);
+            //Act
+            var actual_marques = controller.GetMarques().Result;
+            //Assert
+            CollectionAssert.AreEqual(actual_marques.Value.ToList(), marques, "Les listes ne correspondent pas");
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            mockRepository = null;
+            controller = null;
+        }
+
+    }
+}
