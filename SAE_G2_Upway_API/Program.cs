6878cc063f35165a1f12602defc1035773ed72d2
Stapleton Alexis <alexis.stapleton@etu.univ-smb.fr>
2025-03-26 10:10:46 +0100

    oui

--- SAE_G2_Upway_API/Program.cs
+++ SAE_G2_Upway_API/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddDbContext<UpwayDBContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("UpwayDBContext")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ServerDBContext")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
