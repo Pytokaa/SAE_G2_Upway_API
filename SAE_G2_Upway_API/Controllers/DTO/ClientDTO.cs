11eb39de51b3d7f39811f5adf2d0fb6c60a3bc83
Pytokaa <elioth.rob@proton.me>
2025-04-07 11:31:07 +0200

    correction du postclient

--- SAE_G2_Upway_API/Controllers/DTO/ClientDTO.cs
+++ SAE_G2_Upway_API/Controllers/DTO/ClientDTO.cs
@@ -5,20 +5,21 @@ namespace SAE_G2_Upway_API.Controllers.DTO;
 
 public class ClientDTO
 {
-    public string Nom { get; set; }
-    public string Prenom { get; set; }
-    public string Mail { get; set; }
+    public string Nomclient { get; set; }
+    public string Prenomclient { get; set; }
+    public string Mailclient { get; set; }
     public string Telephone { get; set; }
     public string Password { get; set; }
     public string? UserRole { get; set; }
     
     public ClientDTO(Client client)
     {
-        this.Nom = client.Nomclient;
-        this.Prenom = client.Prenomclient;
-        this.Mail = client.Mailclient;
+        this.Nomclient = client.Nomclient;
+        this.Prenomclient = client.Prenomclient;
+        this.Mailclient = client.Mailclient;
         this.Telephone = client.Telephone;
         this.Password = client.Password;
         this.UserRole = client.UserRole;
     }
+    public ClientDTO(){}
 }
